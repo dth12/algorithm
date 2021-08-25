63aea7cbeed4ead7e91e7586a676173480a8e0d8
email <tlsdh1111@gmail.com>
2021-08-26 02:59:19 +0900

    algo 0826

---
+++ baekjoon/14554_G1_The Other Way.py
@@ -0,0 +1,37 @@
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+
+def dijk() -> int:
+    dist = [INF for _ in range(N + 1)]
+    path = [0 for _ in range(N + 1)]
+    dist[S] = 0
+    path[S] = 1
+    heap = [[0, S]]
+    while heap:
+        min_val, v = heappop(heap)
+        if min_val > dist[v]: continue
+
+        for cost, w in graph[v]:
+            new_cost = dist[v] + cost
+            if new_cost < dist[w]:
+                heappush(heap, [new_cost, w])
+                dist[w] = new_cost
+                path[w] = path[v]
+            elif dist[w] == new_cost:
+                path[w] = (path[w] + path[v]) % 1000000009
+
+    return path[E]
+
+
+N, M, S, E = map(int, input().split())
+INF = 1000000000 * 100000 + 1
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    start, end, cost = map(int, input().split())
+    graph[start].append([cost, end])
+    graph[end].append([cost, start])
+
+print(dijk() % 1000000009)
+
