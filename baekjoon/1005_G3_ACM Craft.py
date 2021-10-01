b9849b90ecf555644f5a4f839b820d46392c8b05
dongyun <tlsdh1111@gmail.com>
2021-10-01 22:33:24 +0900

    algo 1001 ACM craft

--- baekjoon/1005_G3_ACM Craft.py
+++ baekjoon/1005_G3_ACM Craft.py
@@ -1,81 +1,94 @@
-from collections import deque
-
-
-def find_leaf(node: int) -> None:
-    if node in leaf:
-        start.add(node)
-        return
-    else:
-        for v in order[node][0]:
-            find_leaf(v)
-            all_node.add(v)
-
-
-def is_next(node: int) -> bool:
-
-    for v in order[node][0]:
-        if v not in visited:
-            return False
-
-    return True
-
 T = int(input())
 for _ in range(T):
     N, K = map(int, input().split())
     times = list(map(int, input().split()))
-    order = [[set(), set()] for _ in range(N)]
-    leaf = set()
+    times = [0] + times
+    condition = [[] for _ in range(N + 1)]
 
     for _ in range(K):
         f, s = map(int, input().split())
-        order[s - 1][0].add(f - 1)
-        order[f - 1][1].add(s - 1)
+        condition[s].append(f)
 
-    last = int(input()) - 1
+    target = int(input())
+    temp = [target]
+    all_built = {target}
+    while True:
+        cnt = 0
+        next_temp = []
+        for v in temp:
+            for w in condition[v]:
+                if w in all_built: continue
+                next_temp.append(w)
+                all_built.add(w)
 
-    for v in range(N):
-        if len(order[v][0]) == 0:
-            leaf.add(v)
+        if next_temp:
+            temp = next_temp
+        else:
+            break
 
-    if last in leaf:
-        print(times[last])
-    else:
-        if _ == 2:
-            debug = True
-        start = set()
-        all_node = {last}
-        find_leaf(last)
-        answer = times[last]
-        next_nodes = {i for i in start}
-        visited = {i for i in start}
+    already_built = set()
+    time = 0
+    record_times = [0 for _ in range(N + 1)]
+    while True:
+        now = set()
+        for v in all_built:
+            if v not in already_built:
+                flag = True
+                for w in condition[v]:
+                    if w not in already_built:
+                        flag = False
+                        break
 
-        if times[0] == 100000:
-            debug = True
+                if flag:
+                    now.add(v)
 
-        while True:
-            temp = set()
-            stay = set()
-            lazy_visited = set()
-            max_time = 0
-            for v in next_nodes:
-                if not is_next(v):
-                    stay.add(v)
-                    continue
-                max_time = max(times[v], max_time)
-                lazy_visited.add(v)
-                for w in order[v][1]:
-                    if w in all_node:
-                        temp.add(w)
+        if target in now:
+            break
 
-            for v in lazy_visited:
-                visited.add(v)
+        for v in now:
+            already_built.add(v)
+            temp_time = 0
+            for w in condition[v]:
+                temp_time = max(record_times[w], temp_time)
 
-            if last in visited:
-                print(answer)
-                break
+            record_times[v] = temp_time + times[v]
+            time = max(time, record_times[v])
 
-            next_nodes = {i for i in temp.union(stay)}
-            answer += max_time
+    print(time + times[target])
+    # 실패 이유 :
+    # 만약 동시에 짓는 건물을 짓던 중, 하나가 끝나고
+    # 다른 하나를 더 지을 수 있는 경우를 생각하지 못함.
+    # answer = times[target]
+    # while True:
+    #     now = set()
+    #     for v in all_built:
+    #         if v not in already_built:
+    #             flag = True
+    #             for w in condition[v]:
+    #                 if w not in already_built:
+    #                     flag = False
+    #                     break
+    #
+    #             if flag:
+    #                 now.add(v)
+    #
+    #     if target in now:
+    #         break
+    #
+    #     max_time = 0
+    #     for v in now:
+    #         already_built.add(v)
+    #         max_time = max(max_time, times[v])
+    #
+    #     answer += max_time
+    #
+    # print(answer)
+
+
+
+
+
+
 
 
 
